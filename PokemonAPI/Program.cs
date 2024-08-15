78f5acb5a9404dbf7c9ead7568437d80ef330527
maicol <mailopezsantos1998@gmail.com>
2024-08-14 19:10:02 -0600

    Add project files.

---
+++ PokemonAPI/Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.EntityFrameworkCore;
+using PokemonAPI;
+using PokemonAPI.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddTransient<Seed>();
+
+//The code is registering the DataContext class as a service in the application's dependency injection container. 
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+
+//Nosavemo que es esto ni chagpt no dice pero funciona
+if (args.Length == 1 && args[0].ToLower() == "seeddata")
+SeedData(app);
+
+void SeedData(IHost app)
+{
+    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+    using (var scope = scopedFactory.CreateScope())
+    {
+        var service = scope.ServiceProvider.GetService<Seed>();
+        service.SeedDataContext();
+    }
+}
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
