44ae063c877f90918492b4b816313b9b7694aa97
maicol <mailopezsantos1998@gmail.com>
2024-08-15 18:33:53 -0600

    Creation of Interface and Repository only for Pokemon

---
+++ PokemonAPI/Repository/PokemonRepository.cs
@@ -0,0 +1,14 @@
+﻿using PokemonAPI.Data;
+
+namespace PokemonAPI.Repository
+{
+    public class PokemonRepository
+    {
+        private readonly DataContext _context;
+
+        public PokemonRepository(DataContext context)
+        {
+            _context = context;
+        }
+    }
+}
